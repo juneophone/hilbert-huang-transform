0405feac4fae2c53851872877fa846ea1985e6fe
ahemoh <ahemoh@4120e32a-3c06-ef4b-c48e-5fdb73b588fc>
2011-10-20 10:15:34 +0000

    Добавление проекта

---
+++ trunk/src/hht/HHT.java
@@ -0,0 +1,13 @@
+package hht;
+/**
+ *
+ * @author AHEMOH
+ * @author MELONiE
+ * Пакет реализует метод ...
+ */
+public class HHT {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Gsim!!!!!!! I'm nekofaaaaaaaan!!!! =^.^=");
+    }
+}
